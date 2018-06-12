d47268578f158c25f45c412a26ae01168c77aa3b
CraigMacIvor <Craig MacIvor>
2018-06-12 14:39:41 -0400

    Add project files.

---
+++ ThroughEveryAge/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThroughEveryAge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
